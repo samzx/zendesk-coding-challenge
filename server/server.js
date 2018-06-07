17ab8ef596a3f925ac6a24e1682fed05f717cc31
Sam Xie <samxie@outlook.com.au>
2018-06-07 19:29:22 +1000

    implemented basic server side to fetch zendesk api data for a root route

--- server/server.js
+++ server/server.js
@@ -1,15 +1,44 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const base64 = require('base-64');
 
 const app = express();
 
 const server = app.listen(8081, () => {
 
-  const host = server.address().address
-  const port = server.address().port
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+        });
 
-  console.log("Example app listening at http://%s:%s", host, port)
-  app.get('/', (req, res) => {
-    res.send('Hello Guy!');
-  });
+    const port = server.address().port
+
+    console.log("Server app listening on port", port)
+
+    const url = 'https://solexstudios.zendesk.com';
+    const api = '/api/v2/tickets.json';
+    const username = "team@solexstudios.com";
+    const password = "solexstudios";
+
+    const auth = 'Basic ' + base64.encode(username + ":" + password);
+
+    app.get('/', (req, res) => {
+        fetch(url + api , {
+            method: 'GET',
+            headers: {
+                "Authorization": auth
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            res.send(json);
+        })
+        .catch((e) => {
+            res.send(e);
+        })
+    });
 
 })
