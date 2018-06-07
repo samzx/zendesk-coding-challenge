6d9065832f71c6671a711e71c7fa7b85497f3b0e
Sam Xie <samxie@outlook.com.au>
2018-06-07 19:29:53 +1000

    implemented basic react front-end that fetches data from back-end and displays it

--- src/app.js
+++ src/app.js
@@ -4,6 +4,39 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const url = 'http://localhost:8081/';
 
+class App extends React.Component {
+    state = {
+        data: null
+    }
 
-ReactDOM.render(<p>Hello World</p>, document.getElementById("app"));
+    componentDidMount() {
+        fetch(url, {
+            method: 'GET',
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((json) => {
+            console.log(json);
+            this.setState({ data: json });
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                {
+                    this.state.data ? JSON.stringify(this.state.data) : "Loading..."
+                }
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
