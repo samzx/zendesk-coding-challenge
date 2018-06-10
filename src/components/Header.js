a485a5659a0b2e90dd6f1d55e677b6369f8c2e6b
Sam Xie <samxie@outlook.com.au>
2018-06-10 23:36:37 +1000

    header to stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header--title">
-          <h1>Zendesk Browser</h1>
-        </div>
+const Header = props => {
+  return (
+    <div className="header">
+      <div className="header--title">
+        <h1>Zendesk Browser</h1>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Header;
