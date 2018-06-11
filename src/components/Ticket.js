c9e2a35bc08d3d564a62868b60250bec76f6cce6
Sam Xie <samxie@outlook.com.au>
2018-06-11 12:23:05 +1000

    reduced ticket to stateless component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,25 +2,15 @@ import React from "react";
 
 import Details from "./Details";
 
-class Ticket extends React.Component {
-  state = {
-    errors: null,
-    loading: false
-  };
-
-  render() {
-    return (
-      <div className="ticket">
-        <div className="ticket--heading">
-          <h2>Ticket Details</h2>
-        </div>
-        <Details
-          currentTicket={this.props.currentTicket}
-          data={this.props.data}
-        />
+const Ticket = props => {
+  return (
+    <div className="ticket">
+      <div className="ticket--heading">
+        <h2>Details</h2>
       </div>
-    );
-  }
-}
+      <Details currentTicket={props.currentTicket} data={props.data} />
+    </div>
+  );
+};
 
 export default Ticket;
