38bbce3ae6a7d79230a9972ba253c8a7a7bce88a
Sam Xie <samxie@outlook.com.au>
2018-06-11 17:34:38 +1000

    refactored styles scss to draw from variables more

--- src/tests/components/Drawer.test.js
+++ src/tests/components/Drawer.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import Drawer from "../../components/Drawer";
 
-// Using jest-fetch-mock
 describe("<Drawer />", () => {
   it("should render Drawer correctly", () => {
     const wrapper = shallow(<Drawer />);
