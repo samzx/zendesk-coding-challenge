159798a0c76a066f0994501ed25da8ca5ccec623
Sam Xie <samxie@outlook.com.au>
2018-06-11 14:29:37 +1000

    added test cases for List component

--- src/tests/components/List.test.js
+++ src/tests/components/List.test.js
@@ -2,7 +2,17 @@ import React from "react";
 import { shallow } from "enzyme";
 import List from "../../components/List";
 
-test("should render List correctly", () => {
-  const wrapper = shallow(<List />);
-  expect(wrapper).toMatchSnapshot();
+import threeTickets from "../fixtures/threeTickets";
+
+describe("<List />", () => {
+  it("should render List correctly", () => {
+    const wrapper = shallow(<List />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should generate 3 Listings", () => {
+    const wrapper = shallow(<List />);
+    expect(wrapper.instance().generateList(threeTickets.tickets)).toHaveLength(
+      3
+    );
+  });
 });
---
+++ src/tests/fixtures/threeTickets.js
@@ -0,0 +1,30 @@
+const json = {
+  tickets: [
+    {
+      requester_id: 1,
+      assignee_id: 5,
+      subject: "velit eiusmod reprehenderit officia cupidatat",
+      description:
+        "Aute ex sunt culpa ex ea esse sint cupidatat aliqua ex consequat sit reprehenderit. Velit labore proident quis culpa ad duis adipisicing laboris voluptate velit incididunt minim consequat nulla. Laboris adipisicing reprehenderit minim tempor officia ullamco occaecat ut laborum.\n\nAliquip velit adipisicing exercitation irure aliqua qui. Commodo eu laborum cillum nostrud eu. Mollit duis qui non ea deserunt est est et officia ut excepteur Lorem pariatur deserunt.",
+      tags: ["est", "nisi", "incididunt"]
+    },
+    {
+      requester_id: 2,
+      assignee_id: 8,
+      subject: "excepteur laborum ex occaecat Lorem",
+      description:
+        "Exercitation amet in laborum minim. Nulla et veniam laboris dolore fugiat aliqua et sit mollit. Dolor proident nulla mollit culpa in officia pariatur officia magna eu commodo duis.\n\nAliqua reprehenderit aute qui voluptate dolor deserunt enim aute tempor ad dolor fugiat. Mollit aliquip elit aliqua eiusmod. Ex et anim non exercitation consequat elit dolore excepteur. Aliqua reprehenderit non culpa sit consequat cupidatat elit.",
+      tags: ["labore", "voluptate", "amet"]
+    },
+    {
+      requester_id: 3,
+      assignee_id: 10,
+      subject: "ad sunt qui aute ullamco",
+      description:
+        "Sunt incididunt officia proident elit anim ullamco reprehenderit ut. Aliqua sint amet aliquip cillum minim magna consequat excepteur fugiat exercitation est exercitation. Adipisicing occaecat nisi aliqua exercitation.\n\nAute Lorem aute tempor sunt mollit dolor in consequat non cillum irure reprehenderit. Nulla deserunt qui aliquip officia duis incididunt et est velit nulla irure in fugiat in. Deserunt proident est in dolore culpa mollit exercitation ea anim consequat incididunt. Mollit et occaecat ullamco ut id incididunt laboris occaecat qui.",
+      tags: ["laborum", "mollit", "proident"]
+    }
+  ]
+};
+
+export default json;
