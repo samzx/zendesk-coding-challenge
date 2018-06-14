2d8b1a8801bd0bc48f29a486a1c38b2d89bf4231
Sam Xie <samxie@outlook.com.au>
2018-06-14 19:20:05 +1000

    updated snapshots with data for Dashboard and Drawer components

--- src/tests/components/Dashboard.test.js
+++ src/tests/components/Dashboard.test.js
@@ -2,9 +2,17 @@ import React from "react";
 import { shallow } from "enzyme";
 import Dashboard from "../../components/Dashboard";
 
+import { response } from "../fixtures/response.fixtures";
+
 describe("<Dashboard />", () => {
-  it("should render <Dashboard />", () => {
+  it("should render <Dashboard /> without data", () => {
     const wrapper = shallow(<Dashboard />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render <Dashboard /> with data", () => {
+    const wrapper = shallow(<Dashboard />);
+    wrapper.setState({ data: response });
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/tests/components/Drawer.test.js
+++ src/tests/components/Drawer.test.js
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import Drawer from "../../components/Drawer";
 
+import { response } from "../fixtures/response.fixtures";
+
 describe("<Drawer />", () => {
-  it("should render Drawer correctly", () => {
+  it("should render Drawer without data", () => {
     const wrapper = shallow(<Drawer />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render Drawer with data", () => {
+    const wrapper = shallow(<Drawer data={response} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
