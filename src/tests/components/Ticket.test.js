9a5bf823242ac0886bf86632e805eacf29f1345d
Sam Xie <samxie@outlook.com.au>
2018-06-14 18:59:24 +1000

    updated ticket test snapshot with data

--- src/tests/components/Ticket.test.js
+++ src/tests/components/Ticket.test.js
@@ -2,9 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import Ticket from "../../components/Ticket";
 
+import { response } from "../fixtures/response.fixtures";
+
 describe("<Ticket />", () => {
-  it("should render Ticket correctly", () => {
+  const id = 276;
+  it("should render Ticket correctly without data, without current ticket", () => {
     const wrapper = shallow(<Ticket />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render Ticket correctly without data, with current ticket", () => {
+    const wrapper = shallow(<Ticket currentTicket={id} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render Ticket correctly with data, without current ticket", () => {
+    const wrapper = shallow(<Ticket data={response} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should render Ticket correctly with data, with current ticket", () => {
+    const wrapper = shallow(<Ticket data={response} currentTicket={id} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
